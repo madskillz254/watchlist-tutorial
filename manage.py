3eb38c4170a8c9fc46061e09b5c0dbfb410339ee
mugambi254 <mugambi254@localhost.localdomain>
2019-04-17 08:29:26 +0300

    removed movie review and posted from review table

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from  flask_migrate import Migrate, MigrateCommand
 
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 #test for testing ,development-during development and production -during production
 
 manager = Manager(app)
